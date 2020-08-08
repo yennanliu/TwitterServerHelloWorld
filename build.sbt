95bd6658dc5fc8c157160564d5ad27694d957a13
yennanliu <f339339@gmail.com>
2020-08-08 12:42:00 +0800

    add server/AdvancedServer.scala, update build.sbt, sbt meta

--- build.sbt
+++ build.sbt
@@ -4,10 +4,14 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+resolvers += Resolver.sonatypeRepo("snapshots")
+
 libraryDependencies ++= Seq(
 
 "com.twitter" %% "twitter-server" % "20.7.0", 
 "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.1" % Test
 )
+
+//mainClass := Some("server.BasicServer")
 
 conflictManager := ConflictManager.latestRevision
---
+++ src/main/scala/server/AdvancedServer.scala
@@ -0,0 +1,39 @@
+package server
+
+import com.twitter.conversions.DurationOps._
+import com.twitter.finagle.http.{HttpMuxer, Request, Response, Status}
+import com.twitter.finagle.Service
+import com.twitter.logging.Formatter
+import com.twitter.server.TwitterServer
+import com.twitter.util.{Await, Future, Time}
+import java.net.InetSocketAddress
+
+object AdvancedServer extends TwitterServer {
+
+  val what = flag("what", "hello", "String to return")
+  val addr = flag("bind", new InetSocketAddress(0), "Bind address")
+  val durations = flag("alarms", (1.second, 5.second), "2 alarm durations")
+  val counter = statsReceiver.counter("requests_counter")
+  override def failfastOnFlagsNotParsed: Boolean = true
+
+  val service = new Service[Request, Response] {
+    def apply(request: Request) = {
+      debug("Received a request at " + Time.now)
+      counter.incr()
+      val response = Response(request.version, Status.Ok)
+      response.contentString = what() + "\n"
+      Future.value(response)
+    }
+  }
+
+  def main(): Unit = {
+    // We could create a new http server but in this case we use the
+    // one already started for /admin/* endpoints.
+    // The `TwitterServer` trait exposes an `adminHttpServer` that serve all routes
+    // registered in the HttpMuxer object, we just have to add our own.
+    HttpMuxer.addHandler("/echo", service)
+    HttpMuxer.addHandler("/echo/", service)
+    // And wait on the server
+    Await.ready(adminHttpServer)
+  }
+}
